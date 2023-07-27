8cef98a5c61356b0b9ed5ffdfcd24a431cfc10c9
avinash021997 <avinashdkokate02@gmail.com>
2023-07-27 18:01:07 -0500

    add code changes to main

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-document.getElementById("portfolio-heading").addEventListener("click", function() {
-    alert("Welcome to my portfolio section!");
+document.getElementById("work-history").addEventListener("click", function() {
+    alert("Welcome to my work history section!");
   });
-  
+function toggleContactDetails() {
+  var contactDetails = document.getElementById("contactDetails");
+  if (contactDetails.style.display === "none") {
+    contactDetails.style.display = "block";
+  } else {
+    contactDetails.style.display = "none";
+  }
+}
